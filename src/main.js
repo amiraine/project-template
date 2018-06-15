600ea8ee72b2bd1153f17a884a11936ab467f9f0
Guest User <Guest@Epicodus-3D.local>
2018-06-15 15:01:31 -0700

    add sticky nav

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import './styles/master.css';
+
+var nav = document.getElementById("navigation");
+var body = document.getElementById("body");
+var stuck = false;
+var velcro = getDistance();
+
+function getDistance(){
+  var topDist = nav.offsetTop;
+  return topDist;
+}
+
+window.onscroll = function(){
+  var distance = getDistance() - window.pageYOffset;
+  var offset = window.pageYOffset;
+
+  if((distance <= 0) && !stuck){
+    nav.style.position = 'fixed'
+    nav.style.top = '0';
+    stuck = true;
+  } else if (stuck && (offset<=velcro)){
+    nav.style.position = 'static';
+    stuck = false;
+  }
+}
